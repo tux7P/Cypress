a6624783a63ee6c7c68c65a318eeb1de736b18dd
Rsoj <lkwa1687@tuta.io>
2019-09-19 23:20:50 +0200

    Add couple of tests

--- integration/examples/the_internet.spec.js
+++ integration/examples/the_internet.spec.js
@@ -1,9 +1,25 @@
 /// <reference types="Cypress" />
 
-it('should navigate to the Internet app', () => {
+it('should navigate A/B Test Variation', () => {
     cy.visit('https://the-internet.herokuapp.com')
     
-    cy.get('.heading', {timeout: 8000}).should('have.text', 'Welcome to the-internet')
+    cy.get('.heading').should('have.text', 'Welcome to the-internet')
+    
+    cy.get('h2').should('have.text', 'Available Examples')
   
-    cy.get(':nth-child(4) > a').click()
+    cy.get(':nth-child(1) > a').click()
+
+    cy.get('h3').should('have.text', 'A/B Test Variation 1')
+})
+
+it('should Add/Remove Elements', () => {
+    cy.visit('https://the-internet.herokuapp.com')
+    
+    cy.get('.heading').should('have.text', 'Welcome to the-internet')
+    
+    cy.get('h2').should('have.text', 'Available Examples')
+  
+    cy.get(':nth-child(2) > a').click()
+
+    cy.get('h3').should('have.text', 'Add/Remove Elements')
 })
